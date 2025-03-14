6be7641a9e8ba26cbdf1988229d6c36d02bed216
TheHootsman <87533878+TheHootsman@users.noreply.github.com>
2025-03-14 15:15:31 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@
             offset: '75%'
         });
         $('.wp2').waypoint(function () {
-            $('.wp2').addClass('animated fadeInRight');
+            $('.wp2').addClass('animated fadeInLeft');
         }, {
             offset: '75%'
         });
@@ -33,17 +33,12 @@
             offset: '75%'
         });
         $('.wp7').waypoint(function () {
-            $('.wp7').addClass('animated fadeInDown');
+            $('.wp7').addClass('animated fadeInRight');
         }, {
             offset: '75%'
         });
         $('.wp8').waypoint(function () {
             $('.wp8').addClass('animated fadeInRight');
-        }, {
-            offset: '75%'
-        });
-        $('.wp9').waypoint(function () {
-            $('.wp9').addClass('animated fadeInRight');
         }, {
             offset: '75%'
         });
