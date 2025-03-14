07e0032bb8ba600595b337004bb97a6e7f567f2b
TheHootsman <87533878+TheHootsman@users.noreply.github.com>
2025-03-14 16:25:31 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -147,60 +147,6 @@
             $('#btn-show-content').toggleClass('toggle-map-content');
         });
 
-        /********************** Add to Calendar **********************/
-        var myCalendar = createCalendar({
-            options: {
-                class: '',
-                // You can pass an ID. If you don't, one will be generated for you
-                id: ''
-            },
-            data: {
-                // Event title
-                title: "Pierre and Fine's Wedding",
-
-                // Event start date
-                start: new Date('Aug 23, 2025 14:00'),
-                end: new Date('Aug 24, 2025 02:30'),
-
-                // Event Address
-                address: 'Kellerskopf, Wiesbaden',
-
-                // Event Description
-                description: "We can't wait to see you on our big day!"
-            }
-        });
-
-        $('#add-to-cal').html(myCalendar);
-
-
-        /********************** RSVP **********************/
-        $('#rsvp-form').on('submit', function (e) {
-            e.preventDefault();
-            var data = $(this).serialize();
-
-            $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
-
-            if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-                && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-            } else {
-                $.post('https://script.google.com/macros/s/AKfycbyo0rEknln8LedEP3bkONsfOh776IR5lFidLhJFQ6jdvRiH4dKvHZmtoIybvnxpxYr2cA/exec', data)
-                    .done(function (data) {
-                        console.log(data);
-                        if (data.result === "error") {
-                            $('#alert-wrapper').html(alert_markup('danger', data.message));
-                        } else {
-                            $('#alert-wrapper').html('');
-                            $('#rsvp-modal').modal('show');
-                        }
-                    })
-                    .fail(function (data) {
-                        console.log(data);
-                        $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-                    });
-            }
-        });
-
     });
 
     /********************** Extras **********************/
